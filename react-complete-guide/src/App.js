b1776e3ed15466c0f2164c036d71c23ab335d7e5
James McCarron <mccarron.j@gmail.com>
2021-02-18 14:46:11 +1100

    setState practice

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,16 +4,45 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "James", age: 42 },
+      { name: "Asuka", age: 31 },
+      { name: "Chris", age: 39 },
+    ],
+    otherState: "some other value",
+  };
+
+  switchNameHandler = () => {
+    // console.log("Was clicked!");
+    // this.state.persons[0].name = "JimmyMac"; DON'T DO THIS (mutate state directly)
+    this.setState({
+      persons: [
+        { name: "James Michael", age: 42 },
+        { name: "Asuka", age: 31 },
+        { name: "Chris", age: 34 },
+      ],
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!!</p>
-        <Person name="James" age="42" />
-        <Person name="Asuka" age="30">
-          My Hobbies: Scuba-diving
-        </Person>
-        <Person name="Chi-kun" age="12" />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
   }
