1ce664165cdd187ecc87d437dbdbfa68bd40a1b9
James McCarron <mccarron.j@gmail.com>
2021-05-20 15:04:45 +1000

    clickHandler

--- expense-tracker-app/src/components/ExpenseItem/ExpenseItem.js
+++ expense-tracker-app/src/components/ExpenseItem/ExpenseItem.js
@@ -3,6 +3,10 @@ import Card from "../Card/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("Clicked!!!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,7 +14,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button>Change Title</button>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
