0189e0706ed1e35965b8678538f0b0a6927cbab0
James McCarron <mccarron.j@gmail.com>
2021-05-24 16:29:52 +1000

    dropdown and filter handlers

--- expense-tracker-app/src/components/Expenses/Expenses.js
+++ expense-tracker-app/src/components/Expenses/Expenses.js
@@ -1,12 +1,22 @@
+import React, { useState } from "react";
 import ExpenseItem from "../ExpenseItem/ExpenseItem";
 import Card from "../Card/Card";
 import "./Expenses.css";
 import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
-      <ExpensesFilter />
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
