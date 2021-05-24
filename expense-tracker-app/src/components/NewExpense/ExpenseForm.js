49612706acc85a1a740070f4e551b6ec579efaa5
James McCarron <mccarron.j@gmail.com>
2021-05-24 16:34:05 +1000

    props expenseData

--- expense-tracker-app/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker-app/src/components/NewExpense/ExpenseForm.js
@@ -43,7 +43,7 @@ const ExpenseForm = (props) => {
       date: new Date(enteredDate),
     };
 
-    props.onSaveExpenseData();
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
