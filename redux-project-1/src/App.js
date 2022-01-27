8c3a34481c109b8a188eeb95eb963173a52d32f8
James McCarron <mccarron.j@gmail.com>
2022-01-27 12:32:32 +1100

    Changes to JSX transform

--- redux-project-1/src/App.js
+++ redux-project-1/src/App.js
@@ -1,10 +1,11 @@
-import React, { Fragment } from "react";
+import React from "react";
+import { Fragment } from "react";
 
 import Counter from "./components/Counter";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
 
-App = () => {
+const App = () => {
   return (
     <Fragment>
       <Header />
