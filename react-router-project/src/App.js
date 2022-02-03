df60e342b46513346ffb77fc6630e6ee95e31114
James McCarron <mccarron.j@gmail.com>
2022-02-04 10:55:59 +1100

    Created welcome and products component files

--- react-router-project/src/App.js
+++ react-router-project/src/App.js
@@ -1,9 +1,11 @@
-function App() {
+import { Route } from "react-router-dom";
+
+const App = () => {
   return (
     <div>
       <h2>Let's get started!</h2>
     </div>
   );
-}
+};
 
 export default App;
