bd93ce56da9448f6d7b5ace24fc45cd703cb73ff
James McCarron <mccarron.j@gmail.com>
2022-02-03 13:21:03 +1100

    Fetch request with Firebase

--- redux-project-2/src/App.js
+++ redux-project-2/src/App.js
@@ -1,11 +1,20 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 
-function App() {
+const App = () => {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://react-http-f43cf-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
@@ -13,6 +22,6 @@ function App() {
       <Products />
     </Layout>
   );
-}
+};
 
 export default App;
