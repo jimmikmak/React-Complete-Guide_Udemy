ce1785d4bb8c71a7c835fa3f8891f5c6b445d6f3
James McCarron <mccarron.j@gmail.com>
2022-02-05 10:38:32 +1100

    Created new product detail component & added dynamic route

--- react-router-project/src/App.js
+++ react-router-project/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Welcome from "./components/pages/Welcome";
 import Products from "./components/pages/Products";
+import ProductDetail from "./components/pages/ProductDetail";
 import MainHeader from "./components/MainHeader";
 
 const App = () => {
@@ -12,6 +13,10 @@ const App = () => {
         <Routes>
           <Route path="/welcome" element={<Welcome />}></Route>
           <Route path="/products" element={<Products />}></Route>
+          <Route
+            path="/product-detail:productId"
+            element={<ProductDetail />}
+          ></Route>
         </Routes>
       </main>
     </div>
