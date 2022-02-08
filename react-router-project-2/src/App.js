58b4672852851f39900ebde6785e89ae47005612
James McCarron <mccarron.j@gmail.com>
2022-02-09 10:53:54 +1100

    Added layout wrapper component & styling

--- react-router-project-2/src/App.js
+++ react-router-project-2/src/App.js
@@ -3,15 +3,18 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
 import QuoteDetail from "./pages/QuoteDetail";
 import NewQuote from "./pages/NewQuote";
+import Layout from "./components/layout/Layout";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate replace to="/quotes" />} />
-      <Route path="/quotes" element={<AllQuotes />} />
-      <Route path="/quotes/:quoteId" element={<QuoteDetail />} />
-      <Route path="/new-quote" element={<NewQuote />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/quotes" />} />
+        <Route path="/quotes" element={<AllQuotes />} />
+        <Route path="/quotes/:quoteId" element={<QuoteDetail />} />
+        <Route path="/new-quote" element={<NewQuote />} />
+      </Routes>
+    </Layout>
   );
 };
 
