6ad7a8baa1865c16872a442d81eb67d21d82c9ac
James McCarron <mccarron.j@gmail.com>
2022-02-08 15:25:25 +1100

    Added redirect route for /

--- react-router-project-2/src/App.js
+++ react-router-project-2/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 import AllQuotes from "./pages/AllQuotes";
 import QuoteDetail from "./pages/QuoteDetail";
@@ -7,6 +7,7 @@ import NewQuote from "./pages/NewQuote";
 const App = () => {
   return (
     <Routes>
+      <Route path="/" element={<Navigate replace to="/quotes" />} />
       <Route path="/quotes" element={<AllQuotes />} />
       <Route path="/quotes/:quoteId" element={<QuoteDetail />} />
       <Route path="/new-quote" element={<NewQuote />} />
