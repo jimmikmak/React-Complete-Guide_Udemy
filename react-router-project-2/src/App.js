b5b3e6b6afb676d597db992b20706c30abd833b2
James McCarron <mccarron.j@gmail.com>
2022-02-09 12:38:35 +1100

    Added a not found page

--- react-router-project-2/src/App.js
+++ react-router-project-2/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
 import QuoteDetail from "./pages/QuoteDetail";
 import NewQuote from "./pages/NewQuote";
+import NotFound from "./pages/NotFound";
 import Layout from "./components/layout/Layout";
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/quotes" element={<AllQuotes />} />
         <Route path="/quotes/:quoteId" element={<QuoteDetail />} />
         <Route path="/new-quote" element={<NewQuote />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Layout>
   );
