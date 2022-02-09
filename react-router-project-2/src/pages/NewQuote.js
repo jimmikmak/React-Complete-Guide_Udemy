6f9a0bd6573d91f70f2a55fe4c9df5694ea9677b
James McCarron <mccarron.j@gmail.com>
2022-02-09 13:45:36 +1100

    Prompt component not supported by react-router v6

--- react-router-project-2/src/pages/NewQuote.js
+++ react-router-project-2/src/pages/NewQuote.js
@@ -1,11 +1,25 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 const NewQuote = () => {
+  const { sendRequest, status } = useHttp(addQuote);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (status === "completed") {
+      navigate("/quotes");
+    }
+  }, [status, navigate]);
+
   const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
+    sendRequest(quoteData);
   };
 
-  return <QuoteForm onAddQuoteHandler={addQuoteHandler} />;
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
 
 export default NewQuote;
