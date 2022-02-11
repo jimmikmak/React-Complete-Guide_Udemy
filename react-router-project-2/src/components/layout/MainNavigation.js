5ce863d9e040d9adce0b7d467c1541029a5805c8
James McCarron <mccarron.j@gmail.com>
2022-02-12 10:31:26 +1100

    Updated MainNavigation to router v6

--- react-router-project-2/src/components/layout/MainNavigation.js
+++ react-router-project-2/src/components/layout/MainNavigation.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { NavLink } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
@@ -10,12 +9,18 @@ const MainNavigation = () => {
       <nav className={classes.nav}>
         <ul>
           <li>
-            <NavLink to="/quotes" activeClassName={classes.active}>
+            <NavLink
+              to="/quotes"
+              className={(navData) => (navData.isActive ? classes.active : "")}
+            >
               All Quotes
             </NavLink>
           </li>
           <li>
-            <NavLink to="/new-quote" activeClassName={classes.active}>
+            <NavLink
+              to="/new-quote"
+              className={(navData) => (navData.isActive ? classes.active : "")}
+            >
               Add a Quote
             </NavLink>
           </li>
