54c4cb396985a2824376008e6ef0a68426e2785c
James McCarron <mccarron.j@gmail.com>
2022-02-12 11:39:00 +1100

    Finished updating new comments routes in App.js

--- react-router-project-2/src/App.js
+++ react-router-project-2/src/App.js
@@ -1,10 +1,11 @@
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate, Link } from "react-router-dom";
 
 import AllQuotes from "./pages/AllQuotes";
 import QuoteDetail from "./pages/QuoteDetail";
 import NewQuote from "./pages/NewQuote";
 import NotFound from "./pages/NotFound";
 import Layout from "./components/layout/Layout";
+import Comments from "./components/comments/Comments";
 
 const App = () => {
   return (
@@ -12,7 +13,19 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Navigate replace to="/quotes" />} />
         <Route path="/quotes" element={<AllQuotes />} />
-        <Route path="/quotes/:quoteId" element={<QuoteDetail />} />
+        <Route path="/quotes/:quoteId" element={<QuoteDetail />}>
+          <Route
+            path=""
+            element={
+              <div className="centered">
+                <Link className="btn--flat" to={`comments`}>
+                  Load Comments
+                </Link>
+              </div>
+            }
+          />
+          <Route path={`comments`} element={<Comments />} />
+        </Route>
         <Route path="/new-quote" element={<NewQuote />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
